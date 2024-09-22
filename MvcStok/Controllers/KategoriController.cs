4fed21e7422c308f68959c6001dbb55f64bf914a
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-09-22 14:53:41 +0300

    Add project files.

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,19 @@
+﻿using MvcStok.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
